592ec63a85b8bd4e4c7d703654d3434472e65a8f
MarianoRL <42455379+MarianoRL@users.noreply.github.com>
2019-05-06 20:43:54 -0500

    Interfaz e Iconos (Layout)

---
+++ Pinterest/UI.swift
@@ -0,0 +1,22 @@
+//
+//  UI.swift
+//  Pinterest
+//
+//  Created by Alumno on 30/04/19.
+//  Copyright © 2019 MarianoRL. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
+    
+}
