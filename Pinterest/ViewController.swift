7c2ad761e3cb1d6350fab2aaf669060f62e77019
MarianoRL <42455379+MarianoRL@users.noreply.github.com>
2019-05-06 20:26:42 -0500

    Imagenes, Autentificacion Firebase

--- Pinterest/ViewController.swift
+++ Pinterest/ViewController.swift
@@ -10,28 +10,39 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        self.navigationItem.title = "Iniciar Sesión"
         
-        //add subview
+        let backButton = UIBarButtonItem()
+        backButton.title = "Atras"
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        
+        view.backgroundColor = .white
+        
+
         view.addSubview(inputContainerView)
+        
+        view.addSubview(logo)
+        
+        
         view.addSubview(firstButton)
-        inputContainerView.addSubview(nameTextField)
         inputContainerView.addSubview(emailTextField)
         inputContainerView.addSubview(passwordTextField)
         
-        
-        //constraints
-        // constraints for input
+    
         
         inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
         
+        logo.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        logo.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -100).isActive = true
+        logo.heightAnchor.constraint(equalTo: logo.widthAnchor).isActive = true
+        logo.bottomAnchor.constraint(equalTo: inputContainerView.topAnchor, constant: -20).isActive = true
         
         
         firstButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 20).isActive = true
@@ -39,47 +50,40 @@ class ViewController: UIViewController {
         firstButton.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
         firstButton.rightAnchor.constraint(equalTo: inputContainerView.rightAnchor).isActive = true
         
-        nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
-        nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
         
-        
-        emailTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
         emailTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/2).isActive = true
         
         
         passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
         passwordTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
-        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/2).isActive = true
         
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
     let emailTextField : UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Email"
+        tf.placeholder = "Correo Electrónico"
         tf.backgroundColor = .white
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
         return tf
     }()
     
     let passwordTextField : UITextField = {
         let tf = UITextField()
         tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Password"
+        tf.placeholder = "Contraseña"
         tf.backgroundColor = .white
         tf.isSecureTextEntry = true
-        return tf
-    }()
-    
-    let nameTextField : UITextField = {
-        let tf = UITextField()
-        tf.translatesAutoresizingMaskIntoConstraints = false
-        tf.placeholder = "Name"
-        tf.backgroundColor = .white
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 20))
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
         return tf
     }()
     
@@ -94,47 +98,62 @@ class ViewController: UIViewController {
     
     let firstButton : UIButton = {
         let ub = UIButton()
-        ub.backgroundColor =  UIColor(r: 235, g: 101, b: 90)
-        ub.setTitle("Registrarse", for: .normal)
+        ub.backgroundColor = UIColor(r: 201, g: 34, b: 40)
+        ub.setTitle("Iniciar Sesión", for: .normal)
+        ub.titleLabel?.font =  UIFont(name: "GillSans-SemiBold", size: 18)
+        ub.layer.cornerRadius = 20
         ub.translatesAutoresizingMaskIntoConstraints = false
         ub.addTarget(self, action: #selector(handleButton), for: .touchUpInside)
         return ub
     }()
     
+    let logo : UIImageView = {
+        let imageName = "pinterest"
+        let image = UIImage(named: imageName)
+        let iv = UIImageView(image: image!)
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
     @objc func handleButton(){
-        print("Hola Mundo")
-        if let email = emailTextField.text, let pass = passwordTextField.text, let name = nameTextField.text{
-            print(email)
-            print(pass)
-            Auth.auth().createUser(withEmail: email, password: pass) { (data:AuthDataResult?, error) in
+        print("Bienvenido")
+        if let email = emailTextField.text, let pass = passwordTextField.text{            
+            Auth.auth().signIn(withEmail: email, password: pass) { (data:AuthDataResult?, error) in
                 let user = data?.user
                 if error != nil {
                     print(error.debugDescription)
                 }
-                
-                let ref = Database.database().reference(fromURL: "https://pinterest-mrdl98.firebaseio.com")
+        
+                print(email)
+                print(pass)
+                //succesful
+                let ref = Database.database().reference(fromURL: "https://pinterest-mrdl98.firebaseio.com/")
                 
                 if let uid = user?.uid{
-                    
+                
                     let usersRef = ref.child("users").child(uid)
-                    usersRef.updateChildValues(["name" : name, "email" : email, "password": pass])
-
+                    usersRef.updateChildValues(["email" : email, "password": pass])
+                    
+                    //Aqui se crea
                     let msgsRef = ref.child("message").child(uid)
                     msgsRef.updateChildValues(["message" : "Prueba"])
                     
+                    //Aqui se borra el child uid de message, el de "Prueba"
                     ref.child("message").child(uid).removeValue()
                     
                 }
+                
+                let layout = UICollectionViewFlowLayout()
+                layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+                layout.itemSize = CGSize(width: UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.width/2)
+                layout.minimumInteritemSpacing = 0
+                layout.minimumLineSpacing = 0
+                let collectionView:UICollectionViewController = HomeFeedCollectionViewController.init(collectionViewLayout:layout )
+                //let collectionView:UICollectionViewController = HomeFeedCollectionViewController()
+                self.navigationController?.pushViewController(collectionView, animated: true)
             }
         }
     }
     
 }
 
-extension UIColor {
-    
-    convenience init(r:CGFloat, g: CGFloat, b: CGFloat) {
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
-    }
-    
-}
