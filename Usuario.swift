5a2b8e0c860e60f7cfb95ce8df2437084388d187
MarianoRL <42455379+MarianoRL@users.noreply.github.com>
2019-05-06 20:44:20 -0500

    Registro a Firebase

---
+++ Usuario.swift
@@ -0,0 +1,14 @@
+//
+//  Usuario.swift
+//  Pinterest
+//
+//  Created by Alumno on 28/03/19.
+//  Copyright © 2019 MarianoRL. All rights reserved.
+//
+
+import Foundation
+class User {
+    var email: String?
+    var password: String?
+    var age: String?    
+}
